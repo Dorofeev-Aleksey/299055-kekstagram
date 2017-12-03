3373b9f8aa2bb3c598b10053d057a4b5dc935481
Dorofeev-Aleksey <mr.dorick@yandex.ru>
2017-12-03 16:27:49 +0300

    4 задание - 1 вариант

--- js/pictures.js
+++ js/pictures.js
@@ -10,6 +10,7 @@ function getRandomInRange(min, max) {
 
 var photos = [];
 
+//---Формируем массив данных
 for (var i = 0; i < 25; i++) {
   photos[i] = {
     url: 'photos/' + (i + 1) + '.jpg',
@@ -17,7 +18,7 @@ for (var i = 0; i < 25; i++) {
     commets: COMMENTS[getRandomInRange(0, COMMENTS.length - 1)]
   };
 }
-
+//---Создания шаблона и вставка в разметку
 var picturesList = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture-template').content;
 
@@ -39,6 +40,7 @@ for (var j = 0; j < photos.length; j++) {
 
 picturesList.appendChild(fragment);
 
+//---Показ крупного плана, отприсовка картинки крупным планом
 var galleryOverlay = document.querySelector('.gallery-overlay');
 var pictureItems = document.querySelectorAll('.picture');
 var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
@@ -83,8 +85,63 @@ for (i = 0; i < pictureItems.length; i++) {
   });
 }
 
+galleryOverlayClose.addEventListener('click', function (evt) {
+    closePopup();
+});
+
 galleryOverlayClose.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     closePopup();
   }
 });
+
+//--- Взаимодействие с редактором фото
+var uploadInput = document.querySelector('.upload-input');
+var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadFormCancel = document.querySelector('.upload-form-cancel');
+var uploadFormDescription = document.querySelector('.upload-form-description');
+
+var onOverlayEscPress = function (evt) {
+  var focused = document.activeElement;
+  if (evt.keyCode === ESC_KEYCODE) {
+    if (focused === uploadFormDescription) {
+      focused.blur();
+    } else {
+      closeOverlay();
+    }
+  }
+};
+
+var openOverlay = function () {
+  uploadOverlay.classList.remove('hidden');
+  document.addEventListener('keydown', onOverlayEscPress);
+};
+
+var closeOverlay = function () {
+  uploadOverlay.classList.add('hidden');
+  document.removeEventListener('keydown', onOverlayEscPress);
+};
+
+uploadInput.addEventListener('change', function() {
+  openOverlay();
+});
+
+uploadFormCancel.addEventListener('click', function() {
+  closeOverlay();
+});
+
+var effectButtons = document.querySelectorAll('.upload-effect-input');
+var imagePreview = document.querySelector('.effect-image-preview');
+
+var onEffectPress = function (evt) {
+  if (evt.target.checked) {
+    var effectButtonsId = evt.target.id;
+    effectButtonsId= effectButtonsId.substr(7) + '';
+    imagePreview.classList.add(effectButtonsId);
+  }
+};
+
+for (var i = 0; i < effectButtons.length; i++) {
+  var effectButtonsId = effectButtons[i].id;
+  effectButtons[i].addEventListener('change', onEffectPress);
+}
