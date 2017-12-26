45536b7f323725a5817a00e5d356767c91bc2fcd
Dorofeev-Aleksey <mr.dorick@yandex.ru>
2017-12-26 23:16:09 +0300

    Финальная Ñпроверка travis 3

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -5,18 +5,18 @@
 
     var effectButtons = effectContainer.querySelectorAll('.upload-effect-input');
 
-var onEffectPress = function (evt) {
-  if (evt.target.checked) {
-    for (i = 0; i < effectButtons.length; i++) {
-      var newPercent = 100;
-      callback(newPercent);
-      filterDefault(newPercent);
+    var onEffectPress = function (evt) {
+      if (evt.target.checked) {
+        for (i = 0; i < effectButtons.length; i++) {
+          var newPercent = 100;
+          callback(newPercent);
+          filterDefault(newPercent);
 
-      var effectButtonsId = evt.target.id;
-      applyFilter(effectButtonsId);
-    }
-  }
-};
+          var effectButtonsId = evt.target.id;
+          applyFilter(effectButtonsId);
+        }
+      }
+    };
 
     for (var i = 0; i < effectButtons.length; i++) {
       effectButtons[i].addEventListener('change', onEffectPress);
